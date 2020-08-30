689ade551f0c6f4cd3237ba1d8961e3b71e0dab9
willzittlau <willzittlau@outlook.com>
2020-08-30 00:53:48 -0700

    Added basic page transitions

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,7 @@
 <template>
+<div>
   <h1>Hello from About this page</h1>
+</div>
 </template>
 
 <script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
       <h1 class="title">
         Will Zittlau
       </h1>
