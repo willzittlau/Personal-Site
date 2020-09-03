22ae2f54a1ede23998a5a83b3bb983eaa114a64e
willzittlau <willzittlau@outlook.com>
2020-09-03 15:04:39 -0700

    Change index to read all post variables

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -19,7 +19,6 @@
   export default {
     async asyncData({ $content, params }) {
       const articles = await $content('articles', params.slug)
-        .only(['title', 'description', 'img', 'slug'])
         .sortBy('createdAt', 'asc')
         .fetch()
 
