86e85e9ae0907e7207db1e89174abf23c27dbc27
willzittlau <willzittlau@outlook.com>
2020-09-03 23:49:41 -0700

    Added social feed and featured posts

---
+++ components/SocialFeed.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="columns is-mobile">
+    <div class="column"></div>
+    <div class="column is-four-fifths">
+      <h1>Social Feed</h1>
+      <iframe
+        src="//lightwidget.com/widgets/be81feae99b65198bc97a51d2a396594.html"
+        scrolling="no"
+        allowtransparency="true"
+        class="lightwidget-widget"
+        style="width:100%;border:0;overflow:hidden;"
+      ></iframe>
+    </div>
+    <div class="column"></div>
+  </div>
+</template>
+
+<script>
+// };
+export default {
+  mounted() {
+    const plugin = document.createElement("script");
+    plugin.setAttribute(
+      "src",
+      "https://cdn.lightwidget.com/widgets/lightwidget.js"
+    );
+    plugin.async = true;
+    document.head.appendChild(plugin);
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+  font-size: 30px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="container">
+  <div>
     <Hero />
+    <Featured />
+    <SocialFeed />
   </div>
 </template>
 
@@ -15,36 +17,3 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 60px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
